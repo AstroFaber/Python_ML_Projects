eb9989303e6de7735a10502e0eea81b0e23fec97
Emiliano "Faber" Herrera <2FaberVideos@gmail.com>
2023-01-27 18:44:43 -0600

    Update Email_Similarity.py

--- Email_Similarity.py
+++ Email_Similarity.py
@@ -15,6 +15,8 @@ counter.fit(test_emails.data + train_emails.data)
 train_counts = counter.transform(train_emails.data)
 test_counts = counter.transform(test_emails.data)
 
+#building the classifier
 classifier = MultinomialNB()
 classifier.fit(train_counts, train_emails.target)
 print(classifier.score(test_counts, test_emails.target))
+
